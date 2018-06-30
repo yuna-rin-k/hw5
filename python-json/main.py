0c7655d31f8d8ce2b624e4f3fae572afa6ce3f7d
Kris Popendorf <krispop@google.com>
2018-06-30 23:41:30 +0900

    use jinja for rendering

--- python-json/main.py
+++ python-json/main.py
@@ -4,17 +4,31 @@
 import webapp2
 from google.appengine.api import urlfetch
 import json
+import jinja2
 
-networkJson = urlfetch.fetch("http://tokyo.fantasy-transit.appspot.com/net?format=json").content  # ウェブサイトから電車の線路情報をJSON形式でダウンロードする
+networkJson = urlfetch.fetch("https://tokyo.fantasy-transit.appspot.com/net?format=json").content  # ウェブサイトから電車の線路情報をJSON形式でダウンロードする
 network = json.loads(networkJson, 'utf-8')  # JSONとしてパースする（stringからdictのlistに変換する）
+
+tmpl = jinja2.Template(  # Jinjaのテンプレートエンジンを使ってHTMLを作ります
+    u'''
+こういう線路があります：
+<ul>
+{% for line in network %}
+  <li> {{line["Name"]}}
+    <ul>
+    {% for station in line["Stations"] %}
+      <li> {{station}} </li>
+    {% endfor %}
+  </li>
+  </ul>
+{% endfor %}
+</ul>
+''')
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html; charset=UTF-8'
-        self.response.write(u'こういう線路があります：<ul>')
-        for line in network:
-          self.response.write('<li>%s</li>' % line["Name"])
-        self.response.write(u'</ul>')
+        self.response.write(tmpl.render(network=network))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
