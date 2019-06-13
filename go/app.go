f8d080aae90258ef908ee8abf2a2d53faffcc40e
Kris Popendorf <krispop@google.com>
2019-06-13 21:57:45 +0900

    suffix helloworlds

--- go/app.go
+++ go/app.go
@@ -1,15 +1,78 @@
 package app
 
 import (
-	"fmt"
+	"encoding/json"
+	"html/template"
 	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
 )
 
 func init() {
-	http.HandleFunc("/", handlePata)
+	http.HandleFunc("/", handleExample)
+	http.HandleFunc("/norikae", handleNorikae)
 }
 
-func handlePata(w http.ResponseWriter, r *http.Request) {
+// このディレクトリーに入っているすべての「.html」終わるファイルをtemplateとして読み込む。
+var tmpl = template.Must(template.ParseGlob("*.html"))
+
+// Templateに渡す内容を分かりやすくするためのtypeを定義しておきます。
+// （「Page」という名前などは重要ではありません）。
+type Page struct {
+	A string
+	B string
+}
+
+func handleExample(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "Hello world!\n")
+
+	// templateに埋める内容をrequestのFormValueから用意する。
+	content := Page{
+		A: r.FormValue("a"),
+		B: r.FormValue("b"),
+	}
+
+	// example.htmlというtemplateをcontentの内容を使って、{{.A}}などのとこ
+	// ろを実行して、内容を埋めて、wに書き込む。
+	tmpl.ExecuteTemplate(w, "example.html", content)
+}
+
+// LineはJSONに入ってくる線路の情報をtypeとして定義している。このJSON
+// にこの名前にこういうtypeのデータが入ってくるということを表している。
+type Line struct {
+	Name     string
+	Stations []string
+}
+
+// TransitNetworkは http://fantasy-transit.appspot.com/net?format=json
+// の一番外側のリストのことを表しています。
+type TransitNetwork []Line
+
+func handleNorikae(w http.ResponseWriter, r *http.Request) {
+	// Appengineの「Context」を通してAppengineのAPIを利用する。
+	ctx := appengine.NewContext(r)
+
+	// clientはAppengine用のHTTPクライエントで、他のウェブページを読み込
+	// むことができる。
+	client := urlfetch.Client(ctx)
+
+	// JSONとしての路線グラフ内容を読み込む
+	resp, err := client.Get("http://fantasy-transit.appspot.com/net?format=json")
+	if err != nil {
+		panic(err)
+	}
+
+	// 読み込んだJSONをパースするJSONのDecoderを作る。
+	decoder := json.NewDecoder(resp.Body)
+
+	// JSONをパースして、「network」に保存する。
+	var network TransitNetwork
+	if err := decoder.Decode(&network); err != nil {
+		panic(err)
+	}
+
+	// handleExampleと同じようにtemplateにテンプレートを埋めて、出力する。
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tmpl.ExecuteTemplate(w, "norikae.html", network)
 }
