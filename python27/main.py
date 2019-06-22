174e7f08923577f9a002ddef523d7141f4b211c1
Kris Popendorf <krispop@google.com>
2019-06-22 16:09:30 +0900

    replaces hw5 initial reference material with patatokukashi-- content like go and python3

--- python27/main.py
+++ python27/main.py
@@ -6,30 +6,28 @@ from google.appengine.api import urlfetch
 import json
 import jinja2
 
+templateLoader = jinja2.FileSystemLoader(searchpath="templates/") # このディレクトリーからテンプレートを読み込む設定が含まれているオブジェクトを作る。
+templateEnv = jinja2.Environment(loader=templateLoader) # テンプレートを上のtemplateLoaderを使って読み込む環境を用意する。
+pataTmpl = templateEnv.get_template("pata.html") # パタトクカシーー用のテンプレートを"pata.htmlから読み込む。
+networkTmpl = templateEnv.get_template("norikae.html")  # 乗換案内用のテンプレートを"norikae.html"から読み込む。
+
 networkJson = urlfetch.fetch("https://tokyo.fantasy-transit.appspot.com/net?format=json").content  # ウェブサイトから電車の線路情報をJSON形式でダウンロードする
 network = json.loads(networkJson.decode('utf-8'))  # JSONとしてパースする（stringからdictのlistに変換する）
 
-tmpl = jinja2.Template(  # Jinjaのテンプレートエンジンを使ってHTMLを作ります
-    u'''
-こういう線路があります：
-<ul>
-{% for line in network %}
-  <li> {{line["Name"]}}
-    <ul>
-    {% for station in line["Stations"] %}
-      <li> {{station}} </li>
-    {% endfor %}
-  </li>
-  </ul>
-{% endfor %}
-</ul>
-''')
+# このRequestHandlerでパタトカシーーのリクエストを処理して、結果を返す。
+class Pata(webapp2.RequestHandler):
+    def get(self):
+        pata = self.request.get("a") + self.request.get("b")
+        self.response.headers['Content-Type'] = 'text/html; charset=UTF-8'
+        # テンプレートの内容を埋め込んで、返事を返す。
+        self.response.write(pataTmpl.render(pata=pata, request=self.request))
 
-class MainPage(webapp2.RequestHandler):
+class Norikae(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html; charset=UTF-8'
-        self.response.write(tmpl.render(network=network))
+        self.response.write(networkTmpl.render(network=network))
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', Pata),
+    ('/norikae', Norikae),
 ], debug=True)
